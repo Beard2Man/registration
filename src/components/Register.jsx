1b156c802822ef04e73666913a70c71b8c23b34f
Grzegorz Olejarczyk <127106396+Beard2Man@users.noreply.github.com>
2023-10-16 14:45:12 +0200

    registration

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import "../componentsScss/Register.scss";
+import { toast } from "react-toastify";
 
 const Register = () => {
+  const [id, idchange] = useState("");
+  const [secondname, secondnamechange] = useState("");
+  const [password, passwordchange] = useState("");
+  const [email, emailchange] = useState("");
+  const [gender, genderchange] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let regobj = { id, secondname, password, email, gender };
+    console.log(regobj);
+
+    fetch(" http://localhost:8000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(regobj),
+    })
+      .then((res) => {
+        console.log(res);
+        toast.success("Registered successfully.");
+      })
+      .catch((err) => {
+        toast.error("Failed:" + err.message);
+      });
+  };
+
   return (
     <div>
       <div>
-        <form className="container">
+        <form className="container" onSubmit={handleSubmit}>
           <div className="">
             <div className="registrationHeader">
               <h2>Registration</h2>
@@ -13,40 +42,80 @@ const Register = () => {
               <div className="row">
                 <div className="registration-group">
                   <div>
-                    <label htmlFor="">Name:</label>
-                    <input type="text" />
+                    <label>
+                      User Name:<span className="star">*</span>
+                      <input
+                        value={id}
+                        onChange={(e) => idchange(e.target.value)}
+                      ></input>
+                    </label>
                   </div>
                 </div>
                 <div className="registration-group">
                   <div>
-                    <label htmlFor="">Secon Name:</label>
-                    <input type="text" />
+                    <label>
+                      Secon Name:<span className="star">*</span>
+                      <input
+                        value={secondname}
+                        onChange={(e) => secondnamechange(e.target.value)}
+                      ></input>
+                    </label>
                   </div>
                 </div>
                 <div className="registration-group">
                   <div>
-                    <label htmlFor="">Password:</label>
-                    <input type="text" />
+                    <label>
+                      Password:<span className="star">*</span>
+                      <input
+                        value={password}
+                        onChange={(e) => passwordchange(e.target.value)}
+                      ></input>
+                    </label>
                   </div>
                 </div>
                 <div className="registration-group">
                   <div>
-                    <label htmlFor=""></label>
-                    <input type="text" />
+                    <label>
+                      Email:<span className="star">*</span>
+                      <input
+                        value={email}
+                        onChange={(e) => emailchange(e.target.value)}
+                      ></input>
+                    </label>
                   </div>
                 </div>
                 <div className="registration-group">
                   <div>
-                    <label htmlFor=""></label>
-                    <input type="text" />
+                    <label>Gender:</label>
+                    <br />
+                    <label>
+                      Male
+                      <input
+                        checked={gender === "male"}
+                        onChange={(e) => genderchange(e.target.value)}
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        className="gender-group"
+                      ></input>
+                    </label>
+
+                    <label>
+                      Female
+                      <input
+                        onChange={(e) => genderchange(e.target.value)}
+                        checked={gender === "female"}
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        className="gender-group"
+                      ></input>
+                    </label>
                   </div>
                 </div>
-                <div className="registration-group">
-                  <div>
-                    <label htmlFor=""></label>
-                    <input type="text" />
-                  </div>
-                </div>
+              </div>
+              <div>
+                <button type="submit">Register</button>
               </div>
             </div>
           </div>
